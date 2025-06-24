a03119706db449eed428518abd272a2f12df5ba0
khilanlodhi <130900570+khilanlodhi@users.noreply.github.com>
2025-06-24 12:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const loadingdiv = document.getElementById("loading");
+const outputdiv = document.getElementById("output");
+const errordiv = document.getElementById("error");
+
+function downloadImage(url){
+	return new Promise((resolve, reject)=>{
+		const img = new Image();
+		img.src = url;
+
+		img.onload = ()=> resolve(img);
+		img.onerror = ()=> reject(`failed to download : ${url}`);  
+	});
+}
+
+function downloadImages(urls){
+	loadingdiv.style.display = "block";
+	outputdiv.innerHTML = "";
+	errordiv.innerHTML = "";
+
+	const imagepromises = urls.map(obj => downloadImage(obj.url));  
+
+	Promise.all(imagepromises).then(images => {
+
+		loadingdiv.style.display = "none";
+
+		images.forEach(img => {
+			outputdiv.appendChild(img);
+		});
+	})
+	.catch(error=>{
+		loadingdiv.style.display = "none";
+
+		errordiv.textContent = error;
+	});
+}
+
+downloadImages(images);
